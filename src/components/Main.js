15dd99842b2f5b23e1ebe7826d6e9c58e14fac0f
ArielHkust <zhangyue260195@gmail.com>
2017-05-11 12:43:44 +0800

    set stage with locate pics

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //let yeomanImage = require('../images/yeoman.png');
 let imageData = require('../data/imageData.json');
@@ -16,18 +17,160 @@ imageData = (function genImageURL(imgDataArr){
   return imgDataArr;
 })(imageData);
 
+// get random number in range
+function getRangeRandom(low, high){
+  return Math.floor(Math.random() * (high - low) + low);
+}
+
 class AppComponent extends React.Component {
+  constructor() {
+    super();
+    this.Constant = {
+      centerPos: {
+        left: 0,
+        right: 0
+      },
+      hPosRange: {
+        leftSecX: [0,0],
+        rightSecX: [0,0],
+        y: [0,0]
+      },
+      vPosRange: {
+        x: [0,0],
+        topY: [0,0]
+      }
+    };
+    this.state = {
+      imgsArrangeArr: [
+        // {
+        //   pos:{
+        //     left: '0',
+        //     top: '0'
+        //   }
+        // }
+      ]
+    }
+  }
+  // position all images
+  // @param centerIndex: set which img as center image
+  rearrange(centerIndex) {
+    var imgsArrangeArr = this.state.imgsArrangeArr,
+        Constant = this.Constant,
+        centerPos = Constant.centerPos,
+        hPosRange = Constant.hPosRange,
+        vPosRange = Constant.vPosRange,
+        hPosRangeLeftSecX = hPosRange.leftSecX,
+        hPosRangeRightSecX = hPosRange.rightSecX,
+        hPosRangeY = hPosRange.y,
+        vPosRangeTopY = vPosRange.topY,
+        vPosRangeX = vPosRange.x,
+
+        //top image state
+        imgsArrangeTopArr =  [],
+        topImgNum = Math.floor(Math.random() * 2), // select 1\2
+        topImgSpliceIndex = 0,
+
+        //center image state
+        imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex,1);
+        //position center image
+        imgsArrangeCenterArr[0].pos = centerPos;
+        console.log('center');
+        console.log(imgsArrangeCenterArr);
+
+        //find postion state of top img
+        topImgSpliceIndex = Math.floor(Math.random() * (imgsArrangeArr.length - topImgNum));
+        imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
+
+        //position top image
+        imgsArrangeTopArr.forEach(function (value, index){
+          imgsArrangeTopArr[index].pos = {
+            top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
+            left: getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+          };
+        });
+
+        //position images on sides
+        for(let i = 0, j=imgsArrangeArr.length, k=j/2; i<j; i++){
+          var hPosRangeLORX = null;
+          // first half position to left, second half position to right
+          if( i < k){
+            hPosRangeLORX = hPosRangeLeftSecX;
+          } else{
+            hPosRangeLORX = hPosRangeRightSecX;
+          }
+          imgsArrangeArr[i].pos = {
+            top: getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+            left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+          };
+        }
+
+        if(imgsArrangeTopArr && imgsArrangeTopArr[0]) { 
+          imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
+        }
+        imgsArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]); 
+        console.log(imgsArrangeArr);
+
+        this.setState({
+          imgsArrangeArr: imgsArrangeArr
+        });
+
+  }
+
+     //initial position for every images
+  componentDidMount() {
+    var stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+        stageW = stageDOM.scrollWidth,
+        stageH = stageDOM.scrollHeight,
+        halfStageW =  Math.floor(stageW / 2),
+        halfStageH =  Math.floor(stageH / 2);
+    var imgFigureDOM = ReactDOM.findDOMNode(this.refs.imageFigure0),
+        imgW = imgFigureDOM.scrollWidth,
+        imgH = imgFigureDOM.scrollHeight,
+        halfImgW = Math.floor(imgW / 2),
+        halfImgH = Math.floor(imgH / 2);
+    this.Constant.centerPos = {
+      left: halfStageW - halfImgW,
+      top: halfStageH - halfImgH
+    };
+    // position range of left and right component
+    this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+    this.Constant.hPosRange.leftSecX[1] = halfStageW - 3*halfImgW;
+    this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+    this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+    this.Constant.hPosRange.y[0] = -halfImgH;
+    this.Constant.hPosRange.y[1] = stageH - halfImgH;
+    //position range of top component
+    this.Constant.vPosRange.topY[0] = -halfImgH;
+    this.Constant.vPosRange.topY[1] = halfStageH - 3*halfImgH;
+    this.Constant.vPosRange.x[0] = halfStageW - imgW;
+    this.Constant.vPosRange.x[1] = halfStageW;
+
+    console.log(this.Constant);
+
+    this.rearrange(0);
+
+  }
+  
   render() {
     var controllerUnits = [],
         imgFigures = [];
-    imageData.forEach(function(element) {
-      imgFigures.push(<ImgComponent data={element}/>);
-    });
+    imageData.forEach(function(element, index) {
+      if(!this.state.imgsArrangeArr[index]){
+        this.state.imgsArrangeArr[index] = {
+          pos: {
+            left: 0,
+            top: 0
+          }
+        };
+      }
+
+      imgFigures.push(<ImgComponent data={element} ref={'imageFigure'+index} arrange={this.state.imgsArrangeArr[index]}/>);
+    }.bind(this));
     return (
-      <section className = "stage">
+      <section className = "stage" ref="stage">
         <section className = "img-sec">
           {imgFigures}
-        </section>
+        </section> 
         <nav className = "controller-nav">
           {controllerUnits}
         </nav>
@@ -41,8 +184,13 @@ AppComponent.defaultProps = {
 
 class ImgComponent extends React.Component {
   render() {
+    var styleObj = {};
+     //if 'props' have set postion value, use
+    if(this.props.arrange.pos){
+      styleObj = this.props.arrange.pos;
+    }
     return (
-      <figure className = "img-figure">
+      <figure className = "img-figure" style={styleObj}>
         <img src={this.props.data.url} 
              alt={this.props.data.title}
         />
